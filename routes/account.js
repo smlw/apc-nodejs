b5b688f108641e0a25a5a7df5a33b2234564eb04
Alexander Samoylov <smlwmy@gmail.com>
2019-01-17 15:28:29 +0500

    display app settings in the account

--- routes/account.js
+++ routes/account.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const models = require('../models')
 
-router.get('/', (req, res) => {
-    res.render('account', {
-        user: req.user
+
+
+router.get('/', async (req, res) => {
+
+    const apps = await models.App.find({
+        owner: req.user
     });
+
+    console.log(apps)
+
+    res.render('account', {
+        apps,
+        user: req.user
+    })
 });
 
 module.exports = router;
