90f58ea2908e6c3d30555f4a91877efe02df6ceb
Alexander Samoylov <smlwmy@gmail.com>
2019-01-22 19:34:26 +0500

    remove empty models

--- models/logs.js
+++
@@ -1,9 +0,0 @@
-// const adapterMongoDB = require('../adapters/mongodb');
-
-// const Logs = adapterMongoDB.schema.define('Logs', {
-//     appId:              { type: String, required: true},
-//     connectDate:        { type: Date, default: Date.now },
-//     successChange:      { type: Number }
-// });
-
-// module.exports = ('Logs', Logs);
